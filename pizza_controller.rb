f31bbfa5ae84de09e11d024a08cc02398ceebb33
Hanish-H <hamish.h@outlook.com>
2018-05-02 22:48:18 +0100

    added css

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -9,35 +9,27 @@ get '/pizza_orders' do
   @orders = PizzaOrder.all()
   erb(:index)
 end
-
 # CREATE orders
 get '/pizza_orders/new' do
   erb(:new)
 end
-
 # new
 post'/pizza_orders'do
   @order = PizzaOrder.new(params)
   @order.save()
   erb(:create)
 end
-
 # SHOW order
 get '/pizza_orders/:id' do
   @order = PizzaOrder.find(params[:id])
   erb(:show)
 end
-
-
 # UPDATE existing orders
 get '/pizza_orders/:id/edit' do
   @order = PizzaOrder.find(params[:id])
   @order.update()
   redirect to ('pizza_order')
 end
-
-
-
 # DELETE orders
 post '/pizza_orders/:id/delete' do
   @order = PizzaOrder.find(params[:id])
